4f0384a2ef7f1c3ba3e59bb491af7fa159619b97
Sheri Lu <sherisherilu@gmail.com>
2022-10-18 19:51:56 +0000

    add move commands

--- client.js
+++ client.js
@@ -8,12 +8,25 @@ const connect = function () {
   
     // interpret incoming data as text
     conn.setEncoding("utf8");
+
+    conn.on("connect", () => {
+        // code that does something when the connection is first established
+        console.log('Successfully connected to game server');
+        //Snake continues moving every 50ms
+        // setInterval(() => {
+        //     conn.write('Move: up');
+        // }, 50);
+
+        conn.write('Name: SHR');
+      });
   
     return conn;
   };
 
 
   connect();
+
+
 
 
 
